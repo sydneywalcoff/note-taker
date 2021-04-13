10fd6718d8a7af38d3fc4062851b21e8e8797849
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-13 11:58:33 -0700

    route for homepage

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// GET notes page
+app.get('/notes', (req, res) => {
+    res.sendFile('Hello!')
+})
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
