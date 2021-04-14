bce0901e04ad7b8ff6be7344ba4de927aecbbaf5
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-14 09:46:50 -0700

    POST route inserts title but not body of note

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db');
 const path = require('path');
+const createNewNote = require('../../lib');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes);
+    res.json(note);
 });
 
 module.exports = router;
