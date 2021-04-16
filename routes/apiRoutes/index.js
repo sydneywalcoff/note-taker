0a9fd9eee36012074219dd90bd61e93f0a9a7348
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-16 12:18:12 -0700

    began adding the delete route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -5,12 +5,18 @@ const createNewNote = require('../../lib');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
+    console.log(notes);
 });
 
 router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
     const note = createNewNote(req.body, notes);
     res.json(note);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    // res.send('Delete request to the homepage')
+    console.log('You are deleting something');
 });
 
 module.exports = router;
