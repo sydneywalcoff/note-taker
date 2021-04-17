609bd404156e256c8724ad2caefa66bb3b83755a
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-17 15:06:52 -0700

    started psudocode for delete function; otherwise done

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,22 +1,22 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db');
 const path = require('path');
-const createNewNote = require('../../lib');
+const { createNewNote, deleteNote} = require('../../lib');
+var uniqid = require('uniqid');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
-    console.log(notes);
 });
 
 router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
+    req.body.id = uniqid();
     const note = createNewNote(req.body, notes);
     res.json(note);
 });
 
 router.delete('/notes/:id', (req, res) => {
     let id = req.params.id;
-    // res.send('Delete request to the homepage')
+    deleteNote
     console.log('You are deleting something at id: ', id);
 });
 
